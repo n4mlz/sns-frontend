52c7ed8a410b737a160ec8f0e4a4585d317e5153
n4mlz <ohutoncat@gmail.com>
2024-04-24 18:28:09 +0900

    add: middleware for id token

--- src/lib/openapi/index.ts
+++ src/lib/openapi/index.ts
@@ -1,6 +1,23 @@
-import createClient from "openapi-fetch";
+import createClient, { Middleware } from "openapi-fetch";
+import { getAuth } from "firebase/auth";
 import type { paths } from "@lib/openapi/schema";
+import { firebaseApp } from "@lib/firebase";
 import { publicEnv } from "@/constants/env";
 
-const client = createClient<paths>({ baseUrl: publicEnv.BASE_URL });
+const authInterceptor: Middleware = {
+  async onRequest(req, _options) {
+    const auth = getAuth(firebaseApp);
+    const token = await auth.currentUser?.getIdToken();
+    if (!token) {
+      return req;
+    }
+    req.headers.set("Authorization", `Bearer ${token}`);
+    return req;
+  },
+};
+
+const client = createClient<paths>({ baseUrl: publicEnv.API_URL });
+
+client.use(authInterceptor);
+
 export default client;
