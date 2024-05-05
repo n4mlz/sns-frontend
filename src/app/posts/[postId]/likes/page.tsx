350f74f6063277629ffa9a212358ca69007d5639
n4mlz <ohutoncat@gmail.com>
2024-05-06 03:30:05 +0900

    feat: likes page

--- src/app/posts/[postId]/likes/page.tsx
+++ src/app/posts/[postId]/likes/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import path from "path";
 import useSWR from "swr";
 import { Box, Center, Spinner } from "@chakra-ui/react";
 import { useAuthContext } from "@/components/contexts/AuthProvider";
@@ -7,27 +8,27 @@ import Users from "@/components/ui/users";
 import { components } from "@/lib/openapi/schema";
 import Header from "@/components/elements/header";
 
-const RequestsPage = () => {
+const postLikesPage = ({ params }: { params: { postId: string } }) => {
   const authContext = useAuthContext();
 
   const { data, isLoading, mutate } = useSWR<components["schemas"]["user"][]>(
-    authContext.currentUser ? "/api/follows/requests" : null
+    authContext.currentUser ? path.join("/api/posts", params.postId, "/likes") : null
   );
 
   return (
     <>
-      <Header title="リクエスト" />
+      <Header title="いいねした人" />
       {!authContext.currentUser || isLoading ? (
         <Center>
           <Spinner thickness="2px" color="gray.300" margin="40px" />
         </Center>
       ) : data && data.length ? (
         <Box>
-          <Users users={data} usersCallback={(users) => mutate(users, false)} enableReject />
+          <Users users={data} usersCallback={(users) => mutate(users, false)} />
         </Box>
       ) : null}
     </>
   );
 };
 
-export default RequestsPage;
+export default postLikesPage;
