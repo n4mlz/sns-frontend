52b91237926f870a4fe101e99856301c33f8b23d
n4mlz <n4mlz.dev@gmail.com>
2024-12-18 15:54:18 +0900

    update: metadata

--- src/constants/metadata.ts
+++ src/constants/metadata.ts
@@ -1,7 +1,7 @@
 import { publicEnv } from "@constants/env";
 
 export const metaDataConsts = {
-  SITE_NAME: "snooze",
+  SITE_NAME: 'snooze | "鍵垢しか存在しない" 究極のプライバシー重視型 SNS',
   SITE_DESCRIPTION: "静かな世界で愚痴をこぼしたい人へ。鍵垢しか存在しないクローズドなSNS",
   SITE_URL: publicEnv.BASE_URL!,
 };
